f78d1d420c38e8ee202dcd578316043b0f5a266f
= <=>
2022-10-02 14:51:12 +0700

    Xu ly chuoi

---
+++ Net2/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net2.Models;
+
+namespace Net2.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string str)
+        {
+            StringProcess sp = new StringProcess();
+            ViewBag.thongTin = sp.RemoveVietnamese(str);
+            return View();
+        }
+    }
+
+
+}
---
+++ Net2/Models/StringProcess.cs
@@ -0,0 +1,78 @@
+﻿namespace Net2.Models
+{
+    public class StringProcess
+    {
+        public string RemoveRemainingWhiteSpace(string strInput)
+        {
+            string strResult = "";
+            strInput = strInput.Trim();
+            while (strInput.IndexOf("  ") > 0)
+            {
+                strInput = strInput.Replace("  ", " ");
+            }
+            strResult = strInput;
+            return strResult;
+        }
+
+        public string LowerToUpper(string strInput)
+        {
+            return strInput.ToUpper();
+        }
+
+        public string UpperToLower(string strInput)
+        {
+            return strInput.ToLower();
+        }
+
+        public string CapitalizeOneFirstCharacter(string strInput)
+        {
+            return strInput.Substring(0, 1).ToUpper() + strInput.Substring(1);
+        }
+
+        public string CapitalizeFirstCharacter(string strInput)
+        {
+            string result = "";
+            for (int i = 1; i < strInput.Length; i++)
+            {
+                if (strInput[i - 1] == ' ')
+                {
+                    result += strInput.Substring(i, 1).ToUpper();
+                }
+                else
+                {
+                    result += strInput[i];
+                }
+            }
+            return result;
+        }
+
+        public string RemoveVietnamese(string strInput)
+        {
+
+            string[] VietnameseSigns = new string[]
+            {
+                "aAeEoOuUiIdDyY",
+                "áàạảãâấầậẩẫăắằặẳẵ",
+                "ÁÀẠẢÃÂẤẦẬẨẪĂẮẰẶẲẴ",
+                "éèẹẻẽêếềệểễ",
+                "ÉÈẸẺẼÊẾỀỆỂỄ",
+                "óòọỏõôốồộổỗơớờợởỡ",
+                "ÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠ",
+                "úùụủũưứừựửữ",
+                "ÚÙỤỦŨƯỨỪỰỬỮ",
+                "íìịỉĩ",
+                "ÍÌỊỈĨ",
+                "đ",
+                "Đ",
+                "ýỳỵỷỹ",
+                "ÝỲỴỶỸ"
+            };
+            for (int i = 1; i < VietnameseSigns.Length; i++)
+            {
+                for (int j = 0; j < VietnameseSigns[i].Length; j++)
+                    strInput = strInput.Replace(VietnameseSigns[i][j], VietnameseSigns[0][i - 1]);
+            }
+            return strInput;
+        }
+    }
+}
